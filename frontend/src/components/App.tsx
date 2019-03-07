75e5b1557ecbddedfaa1f1a3e738765058719430
rick-mo <35053403+rick-mo@users.noreply.github.com>
2019-03-08 06:28:49 +0900

    upload not done

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,55 +1,23 @@
 import * as React from 'react';
-import axios from 'axios';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import '../css/App.css';
+import ChartPage from './ChartPage';
+import UploadPage from './UploadPage';
 
-interface IChartState {
-  id?: number;
-  title?: string;
-  row_title?: string;
-  column_title?: string;
-}
-
-interface IChartsState {
-  charts?: IChartState[]
-}
-
-class App extends React.Component<IChartsState> {
-  public state: IChartsState = {
-    charts: [
-      {
-        id: 0,
-        title: 'initial',
-        row_title: '1st_row',
-        column_title: '1st_column'
-      },
-    ]
-  }
-
-  componentDidMount() {
-    this.getChart();
-  }
-
-  private getChart() {
-    axios.get('http://localhost:8000/api/')
-      .then(res => {
-        this.setState({ charts: res.data });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+class App extends React.Component<{}> {
 
   public render() {
     return (
-      <div>
-        {this.state.charts!.map(item => (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-            <h3>{item.row_title}</h3>
-            <h3>{item.column_title}</h3>
-          </div>
-        ))}
-      </div>
+      <Router>
+        <ul>
+          <li><Link to="/chart">Chart</Link></li>
+          <li><Link to="/upload">Upload</Link></li>
+        </ul>
+        <div>
+          <Route path="/chart" exact component={ChartPage}/>
+          <Route path="/upload" exact component={UploadPage}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ frontend/src/components/UploadPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import axios from 'axios';
+
+const UploadPage : React.FC = () => {
+  return (
+    <div>
+      <input type="file" />
+    </div>
+  )
+}
+
+export default UploadPage;
